6a904ee1321c1ef95fab1926e42fa52eab062720
Host-1 <host-1@Host-1s-Mac-mini.local>
2017-08-20 17:21:11 +0630

    ok

--- testPrj/ViewController.swift
+++ testPrj/ViewController.swift
@@ -12,9 +12,14 @@ class ViewController: UIViewController {
     var testData = 3
     override func viewDidLoad() {
         super.viewDidLoad()
+     
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //YTHA
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
