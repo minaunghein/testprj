150dd12a17d91420f800ad32b8ce43109879b292
Ngu Hlaing <ngalngal@ngal-macbook-pro.local>
2017-08-20 17:32:43 +0630

    here me

--- testPrj/ViewController.swift
+++ testPrj/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //MSO
+        //NWH
     }
 
     override func didReceiveMemoryWarning() {
