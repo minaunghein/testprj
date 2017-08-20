e71b025617577a7278258d09996c47665b10d808
mic-hledan4 <mic-hledan4@mic-hledan4s-mac-mini.local>
2017-08-20 16:33:32 +0630

    Initial Commit

---
+++ testPrj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testPrj
+//
+//  Created by mic-hledan4 on 8/20/17.
+//  Copyright © 2017 google. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
